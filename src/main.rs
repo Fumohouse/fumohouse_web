6e6081efd48c7c3e181b393d917d1707a3c00337
kyoseki <76502906+kyoseki@users.noreply.github.com>
2022-04-24 17:38:08 -0700

    Begin templating

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,22 @@
-#[macro_use] extern crate rocket;
+#[macro_use]
+extern crate rocket;
 
+use rocket::figment::{
+    util::map,
+    value::{Map, Value},
+};
+use rocket::fs::FileServer;
+use rocket::serde::Serialize;
+use rocket_dyn_templates::Template;
 use std::env;
-use rocket::figment::{value::{Map, Value}, util::map};
+
+#[derive(Serialize)]
+#[serde(crate = "rocket::serde")]
+struct EmptyContext {}
 
 #[get("/")]
-fn index() -> &'static str {
-    "Hello, world!"
+fn index() -> Template {
+    Template::render("index", &EmptyContext {})
 }
 
 #[launch]
@@ -17,16 +28,18 @@ fn rocket() -> _ {
         }
     }
 
-    let database_url = env::var("DATABASE_URL").expect("The DATABASE_URL environment variable is not set.");
+    let database_url =
+        env::var("DATABASE_URL").expect("The DATABASE_URL environment variable is not set.");
 
     let db: Map<_, Value> = map! {
         "url" => database_url.into(),
         "pool_size" => 10.into(),
     };
 
-    let figment = rocket::Config::figment()
-        .merge(("databases", map!["fumohouse_db" => db]));
+    let figment = rocket::Config::figment().merge(("databases", map!["fumohouse_db" => db]));
 
     rocket::custom(figment)
+        .attach(Template::fairing())
+        .mount("/", FileServer::from("static/"))
         .mount("/", routes![index])
 }
